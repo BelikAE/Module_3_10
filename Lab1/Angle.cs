683af92317d17d75224a0d8798856a916eb5359c
BelikAE <BelikAE@HOME-PC>
2024-07-17 20:50:53 +1000

    Выдача

---
+++ Lab1/Angle.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Angle
+    {
+        int gradus;
+        int min;
+        int sec;
+        public bool isCorrect = true;
+        public int Gradus 
+        {
+            get 
+            { 
+                return gradus;
+            }
+            set 
+            {
+                if (value < 0) 
+                    isCorrect = false; 
+                else
+                    gradus = value%360;
+            } 
+        }
+        public int Min
+        {
+            get
+            {
+                return min;
+            }
+            set
+            {
+                if (value < 0 || value > 60) 
+                {
+                    Console.WriteLine("Диапазон минут от 0...60");
+                    isCorrect = false;
+                    
+                    
+                }   
+                else
+                    Gradus += value / 60;
+                    min = value % 60;
+
+            }
+        }
+        public int Sec
+        {
+            get
+            {
+                return sec;
+            }
+            set
+            {
+                if (value < 0 || value > 60)
+                {
+                    Console.WriteLine("Диапазон секунд от 0...60");
+                    isCorrect = false;
+                    
+                }
+                else
+                    sec = value;
+
+
+            }
+        }
+
+        public Angle(int gradus, int min, int sec)
+        {
+            this.Gradus = gradus;
+            this.Min = min;
+            this.Sec = sec;
+        }
+
+        public double ToRadians()
+        {
+            return (gradus + (min + (double)sec / 60) / 60) * Math.PI / 180;
+        }
+
+
+    }
+}
--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -10,6 +10,31 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
+            Angle angle;
+            try
+            {
+                do
+                {
+                    Console.WriteLine("Введите градусы");
+                    int gradus = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Введите минуты");
+                    int min = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Введите секунды");
+                    int sec = Convert.ToInt32(Console.ReadLine());
+
+                    angle = new Angle(gradus, min, sec);
+
+                } while (angle.isCorrect == false);
+                double rad = angle.ToRadians();
+                Console.WriteLine("В радианнах:{0}", rad);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            
+            Console.ReadKey();
         }
     }
 }
